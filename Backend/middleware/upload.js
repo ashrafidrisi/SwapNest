2dbab9d41018a00782e8f2e754d1224e0fde97a7
ashrafidrisi <mohdashrafidrisi@gmail.com>
2026-09-16 20:01:56 +0530

    update SwapNest

--- Backend/middleware/upload.js
+++ Backend/middleware/upload.js
@@ -1,14 +1,40 @@
-const multer = require("multer");
+// const multer = require("multer");
+// const path = require("path");
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   },
+// });
+
+// const upload = multer({ storage });
+// module.exports = upload;
+
+// maine yeah kiya hai uper wale ki jagah per
+
+   const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadDir = path.join(__dirname, "../uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
+
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
 const upload = multer({ storage });
+
 module.exports = upload;
